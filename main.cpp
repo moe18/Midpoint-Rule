ef5296fa2dfe38ec3b40ce478022e7e33258fd8b
moe18 <chabotmoe1@gmail.com>
2018-05-27 12:34:46 -0400

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,27 @@
+
+
+#include <iostream>
+#include <cmath>
+
+double f(double x){
+    return x*x+log(x);
+}
+
+int main() {
+    int l,r;
+    double n;
+    std::cout<<"enter the left and right bound then the number of partitions"<<std::endl;
+    std::cin>>l>>r>>n;
+    double h = (r-l)/n;
+    double mid = 0;
+    
+    for(int i=1;i<=n;++i){
+        mid+= f(l+(i- .5)*h);
+    }
+    mid = h*mid;
+    std::cout<<mid;
+    
+    
+
+    return 0;
+}
